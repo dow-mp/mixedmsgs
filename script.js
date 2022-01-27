53b3621e94334a999af46839daffe4dd91d4ec0d
sharkba1t <87448201+sharkba1t@users.noreply.github.com>
2022-01-27 00:05:08 -0500

    Make button functional

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+//html elements
+const msgButton = document.getElementById('msgButton')
+const messageWindow =  document.getElementById('message')
+
 // load data JSON file
 const data = require('./data.json')
 // push nouns, verbs and adjectives
@@ -13,3 +17,21 @@ data.map(datum => {
 console.log(nouns)
 console.log(verbs)
 console.log(adjectives)
+
+// For message & button animation 
+function buttonDisappears() {
+    msgButton.style.animation = 'fadeOut 1s ease-in 0s'
+    setTimeout(() => {
+        msgButton.style.display = 'none';
+    }, 1000)
+    setTimeout(() => {
+        displayMessage();
+    }, 2000)
+	return;
+}
+
+function displayMessage() {    
+    messageWindow.style.display = 'flex';
+    messageWindow.style.animation ='fadeIn 1s ease-in 0s';
+    
+}
