16d563a83fb85cd3f83f7fca95e53acab17ed91a
dow-mp <derin.mitchell@gmail.com>
2022-01-30 21:15:49 -0500

    added and edited second js file for eventListener functions

---
+++ jsfunctions.js
@@ -0,0 +1,19 @@
+const nouns = ["smile", "eyes", "paper", "project"];
+const verbs = ["amaze", "thrill", "thrive", "crush"];
+const adjectives = ["lively", "dynamic", "playful", "brave", "pleasant"];
+
+let n = Math.floor(Math.random() * (nouns.length-1));
+let v = Math.floor(Math.random() * (verbs.length-1));
+let a = Math.floor(Math.random() * (adjectives.length-1));
+
+const phrases = [`Your ${nouns[n]} will be ${adjectives[a]} today!`, `You will ${verbs[v]} today!`, `Your ${nouns[n]} looks ${adjectives[a]} today.`]
+
+let p = Math.floor(Math.random() * 3);
+
+function randomPhrase() {
+    //console.log(phrases[p]);
+    document.getElementById("msg").innerHTML = phrases[p];
+    document.getElementById("msgButton").removeEventListener("click", randomPhrase);
+};
+
+document.getElementById("msgButton").addEventListener("click", randomPhrase);
